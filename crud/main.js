cd42d9de4c60ee13918ad2ad7dcbea7e659edaed
Arpan Sharma <arpansharma.aipl@gmail.com>
2025-07-11 16:35:38 +0530

    Setup paths of view files and remove unused commenting

--- crud/main.js
+++ crud/main.js
@@ -2,71 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const path = require('path');
-
 const app = express();
-// app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
-
-// // Create User
-// app.get('/employee', (req, res) => { 
-//     const {name, email} = req.query;
-//     const sql = 'INSERT INTO employee (name, email) VALUES (Anshu, anshu@gmail.com)';
-//     db.query(sql, [name, email], (err, result) => { 
-//         if (err) throw err;
-//         res.send({id: result.insertId, name, email});
-//     });
-// });
-// // Read All employee
-// app.get('/read', (req, res) => {
-//     const sql = 'SELECT * FROM employee';
-//     db.query(sql, (err, results) => {
-//       if (err) throw err;
-//       res.send(results);
-//     });
-//   });
-  
-//   // Read Single User
-//   app.get('/employee/:id', (req, res) => {
-//     const sql = 'SELECT * FROM employee WHERE id = ';
-//     db.query(sql, [req.params.id], (err, result) => {
-//       if (err) throw err;
-//       res.send(result[0] || {});
-//     });
-//   });
-  
-//   // Update User 
-//   app.put('/employee/:id', (req, res) => {
-//     const { name, email } = req.body;
-//     const sql = 'UPDATE employee SET name = , email =  WHERE id = ';
-//     db.query(sql, [name, email, req.params.id], (err, result) => {
-//       if (err) throw err;
-//       res.send({ message: 'User updated' });
-//     });
-//   });
-  
-//   // Delete User
-//   app.delete('/employee/:id', (req, res) => {
-//     const sql = 'DELETE FROM employee WHERE id = ';
-//     db.query(sql, [req.params.id], (err, result) => {
-//       if (err) throw err;
-//       res.send({ message: 'User deleted' });
-//     });
-//   });
-  
-//   app.listen(3000, () => {
-//     console.log('Server running on port 3000');
-//   });
-
+app.set('views', path.join(__dirname, '../crud/views'));
 
 
 //  Show All employee
 app.get('/employee', (req, res) => {
     db.query('SELECT * FROM employee', (err, results) => {
       if (err) throw err;
-      res.render('employee/index', { employees: results });
+      res.render('employee/index', { employee: results });
     });
   });
   
